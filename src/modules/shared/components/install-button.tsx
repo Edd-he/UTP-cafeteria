0033c1eac2fe1bff1e996661c4027982f34194b1
EddieHermoza <eddie.ehc04@gmail.com>
2025-05-08 01:21:56 -0500

    chore: remove unused image files from public directory

--- src/modules/shared/components/install-button.tsx
+++ src/modules/shared/components/install-button.tsx
@@ -1,72 +1,72 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 import { useEffect, useState } from 'react'
 import { BsDownload } from 'react-icons/bs'
 
-import { useMediaQuery } from '@shared/hooks/use-media-query'
-
 import { Button } from './ui/button'
+import { useMediaQuery } from '../hooks/use-media-query'
 
 const InstallButton = () => {
-  const [installPrompt, setInstallPrompt] = useState<any>(null)
-  const [isStandalone, setIsStandalone] = useState(false)
-  const [isCompatible, setIsCompatible] = useState(false)
-  const isInStandalone = useMediaQuery('(display-mode: standalone)')
+    const [installPrompt, setInstallPrompt] = useState<any>(null)
+    const [isStandalone, setIsStandalone] = useState(false)
+    const [isCompatible, setIsCompatible] = useState(false)
+    const isInStandalone = useMediaQuery('(display-mode: standalone)')
 
-  useEffect(() => {
-    const isInStandaloneMode =
-      isInStandalone || (window.navigator as any).standalone === true
+    useEffect(() => {
 
-    setIsStandalone(isInStandaloneMode)
+        const isInStandaloneMode = isInStandalone  || (window.navigator as any).standalone === true
 
-    if ('serviceWorker' in navigator) {
-      setIsCompatible(true)
-    }
+        setIsStandalone(isInStandaloneMode)
 
-    const handleBeforeInstallPrompt = (event: any) => {
-      event.preventDefault()
-      setInstallPrompt(event)
-    }
-
-    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt)
-
-    return () =>
-      window.removeEventListener(
-        'beforeinstallprompt',
-        handleBeforeInstallPrompt
-      )
-  }, [isInStandalone])
-
-  const handleInstallClick = () => {
-    if (installPrompt) {
-      installPrompt.prompt()
-      installPrompt.userChoice.then((choiceResult: any) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.warn('El usuario aceptó la instalación')
-        } else {
-          console.warn('El usuario rechazó la instalación')
+        if ('serviceWorker' in navigator) {
+            setIsCompatible(true)
         }
-        setInstallPrompt(null)
-      })
+
+        const handleBeforeInstallPrompt = (event: any) => {
+            event.preventDefault()
+            setInstallPrompt(event)
+        }
+
+        window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt)
+
+        return () =>
+            window.removeEventListener(
+                'beforeinstallprompt',
+                handleBeforeInstallPrompt
+            )
+    }, [isInStandalone])
+
+    const handleInstallClick = () => {
+        if (installPrompt) {
+            installPrompt.prompt()
+            installPrompt.userChoice.then((choiceResult: any) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.warn('El usuario aceptó la instalación')
+                } else {
+                    console.warn('El usuario rechazó la instalación')
+                }
+                setInstallPrompt(null)
+            })
+        }
     }
-  }
 
-  if (isStandalone || !isCompatible) return null
+    if (isStandalone || !isCompatible) return null
 
-  return (
-    <>
-      {installPrompt && (
-        <Button
-          onClick={handleInstallClick}
-          variant={'outline'}
-          size={'icon'}
-          className="duration-200"
-        >
-          <BsDownload size={18} />
-        </Button>
-      )}
-    </>
-  )
+    return (
+        <>
+            {installPrompt && (
+                <Button
+                    onClick={handleInstallClick}
+                    variant={'outline'}
+                    size={'icon'}
+                    className="duration-200"
+                >
+                    <BsDownload size={18} />
+                </Button>
+            )}
+        </>
+    )
 }
 
 export default InstallButton
