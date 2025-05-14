4e1da078ca3b51ccad36ccc98ee69600f6b5b5b6
EddieHermoza <eddie.ehc04@gmail.com>
2025-05-14 01:43:01 -0500

    chore: add console warning for debugging in cart-store

--- src/store/cart-store.ts
+++ src/store/cart-store.ts
@@ -3,7 +3,7 @@ import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
 import { CartProduct } from '@/modules/shared/interfaces/products.interfaces'
-
+console.warn('zuztand')
 type CartState = {
   cart: CartProduct[]
 
